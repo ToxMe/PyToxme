bf6872964d96152b8c6e89a294c2f9c148e2435b
Sean <sean@tox.im>
2014-08-14 00:31:37 -0700

    simple_functions

--- PyToxme/pytoxme.py
+++ PyToxme/pytoxme.py
@@ -67,19 +67,48 @@ def nonce():
 	return nacl.utils.random(Box.NONCE_SIZE)
 
 def getbox(auth,key):
-	key_raw = nacl.public.PublicKey(key, encoder=nacl.encoding.HexEncoder)
-	return Box(auth,key_raw)
+	try:
+		key_raw = nacl.public.PublicKey(key, encoder=nacl.encoding.HexEncoder)
+		try:
+			return Box(auth,key_raw)
+		except:
+			raise err.api('invalid auth object')
+	except:
+		raise err.api('invalid server key')
 
 def payload_push(box,auth,nonce,tox_id,name,privacy=1,bio=''):
 	payload = '{{"tox_id": "{}", "name":"{}", "privacy":{},"bio":"{}","timestamp":{}}}'.format(tox_id,name,privacy,bio,int(time.time()))
-	return box.encrypt(payload,nonce,encoder=nacl.encoding.Base64Encoder).ciphertext
+	try:
+		return box.encrypt(payload,nonce,encoder=nacl.encoding.Base64Encoder).ciphertext
+	except:
+		raise err.api('Error encrypting payload.')
 
 def payload_delete(box,auth,nonce,tox_id):
 	payload = '{{"public_key": "{}","timestamp":{}}}'.format(tox_id[0:64],int(time.time()))
-	return box.encrypt(payload,nonce,encoder=nacl.encoding.Base64Encoder).ciphertext
+	try:
+		return box.encrypt(payload,nonce,encoder=nacl.encoding.Base64Encoder).ciphertext
+	except:
+		raise err.api('Error encrypting payload.')
 
-def push(domain,payload,auth,r_nonce):
-	return _toxme_err(_pushauth(1,domain,payload,auth,r_nonce))
+
+def push(domain,payload,auth,nonce):
+	return _toxme_err(_pushauth(1,domain,payload,auth,nonce))
 
 def delete(domain,payload,auth,r_nonce):
-	return _toxme_err(_pushauth(2,domain,payload,auth,r_nonce))
+	return _toxme_err(_pushauth(2,domain,payload,auth,nonce))
+
+def simple_push(domain,name,toxid,secret='',privacy=1,bio=''):
+	pk = getpub(domain) 
+	auth = getauth(secret)
+	crypto = getbox(auth,pk)
+	nonce = nonce();
+	payload = payload_push(crypto,auth,nonce,toxid,name,privacy,bio)
+	return push(domain,payload,auth,nonce)
+
+def simple_delete(domain,toxid,secret):
+	pk = getpub(domain) 
+	auth = getauth(secret)
+	crypto = getbox(auth,pk)
+	nonce = nonce();
+	payload = payload_delete(crypto,auth,nonce,toxid)
+	return delete(domain,payload,auth,nonce)
